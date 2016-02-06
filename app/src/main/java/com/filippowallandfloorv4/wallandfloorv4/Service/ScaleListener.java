c44c5332b8e3dbeab101ddb589dd30cd28d7d053
Casual70 <casual70@gmail.com>
2016-02-06 13:27:08 +0100

    GridView Main Activity: - aggiungere sotto l'image view una banda semitrasparente dove annotare se l'immagine è già stata editata o è originale (forse sarà necessario agire sul database o decodificando la dimesione in byte dell'immagine) ListView Project Main Activity: -migliorare la grafica e il layout (OnWork) - se possibile ampliare la listview con un expandablelistview dove far uscire le zone del progetto (fatto ma migliorare il layout) --l'Header si riprosiziona ogni volta che parte Onstart() fare in modo che si posizioni una volta soltanto -implementare il longClick per eliminare i project dalla lista e lo stesso per le immagini (anche dal database ed eliminare i file) ViewFor DrawIn -risolvere il problema dell Undo/Redo (Dopo l'undo non posso ripetere il flood fill sulle stessa zona - credo sia meglio agire sul backBitmap) -- fatto -sistemare la backImage in modo che i che vada settata solo all'occorenza e non per ogni contorno -appena si clicca nell'editor avvisare se la backImage non è stata settata (in tal caso invitare a farlo) -- per  i dettagli implementare la possibilità di fare zoom sulla foto e nel metodo OnDraw con la linea impedire il disegno se il backImage è già stato editato in quel punto (vedere dei Layer)
      ON WORK
    -vedi tood nel mainActivity nel dialog
    
    Finire lo zoom
    http://www.tutorialspoint.com/android/android_gestures.htm

--- app/src/main/java/com/filippowallandfloorv4/wallandfloorv4/Service/ScaleListener.java
+++ app/src/main/java/com/filippowallandfloorv4/wallandfloorv4/Service/ScaleListener.java
@@ -5,5 +5,9 @@ import android.view.ScaleGestureDetector;
 /**
  * Created by Filippo on 05/02/2016.
  */
-public class ScaleListener extends ScaleGestureDetector {
+public class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+    @Override
+    public boolean onScale(ScaleGestureDetector detector) {
+        return super.onScale(detector);
+    }
 }
